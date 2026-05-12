6df6a4be66dcd29b5f0958997881be79fb3f523f
Chen Noylander <chen.noylander@aquasec.com>
2026-05-12 21:52:53 +0300

    Add getTailNodes() that returns nodes that are an end/tail of a route

--- src/infrastructure/graph-query.ts
+++ src/infrastructure/graph-query.ts
@@ -14,3 +14,12 @@ export function getRootNodes(graph: Graph): GraphNode[] {
 
     return graph.nodes.filter(node => !toNodes.has(node.name));
 }
+
+// getTailNodes returns nodes that are an end/tail of a route
+export function getTailNodes(graph: Graph): GraphNode[] {
+    console.info("Getting tail nodes");
+    const fromNodes = new Set(graph.edges.map(e => e.from));
+    
+    // A tail is any node that doesn't originate an edge
+    return graph.nodes.filter(node => !fromNodes.has(node.name));
+}
