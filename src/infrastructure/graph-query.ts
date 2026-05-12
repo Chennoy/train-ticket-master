660d3a941dc8f9e65a6e31e63f6212e08c32cb62
Chen Noylander <chen.noylander@aquasec.com>
2026-05-12 21:50:42 +0300

    Fix getRootNodes() to filter only by to (and not by from) to not miss any isolated node

--- src/infrastructure/graph-query.ts
+++ src/infrastructure/graph-query.ts
@@ -4,19 +4,13 @@ import type { GraphNode } from "../models/node/node-types";
 // getRootNodes returns nodes that are a head/root of a route
 export function getRootNodes(graph: Graph): GraphNode[] {
     console.info("Getting root nodes");
-    const fromNodes = new Set<string>();
     const toNodes = new Set<string>();
-
+    
     for (const edge of graph.edges) {
-        fromNodes.add(edge.from);
         for (const destination of edge.to) {
             toNodes.add(destination);
         }
     }
 
-    return graph.nodes.filter(
-        (node) =>
-            fromNodes.has(node.name) &&
-            !toNodes.has(node.name)
-    );
+    return graph.nodes.filter(node => !toNodes.has(node.name));
 }
