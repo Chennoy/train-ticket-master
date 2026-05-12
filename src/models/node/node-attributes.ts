e9db201e56a488f8feb3389b3d9b0850ddd5208e
Chen Noylander <chen.noylander@aquasec.com>
2026-05-12 21:34:27 +0300

    Add node-attributes.ts that exposes the GraphNode attributes: hasVulnerabilities(), isSink() and isPublic()

---
+++ src/models/node/node-attributes.ts
@@ -0,0 +1,17 @@
+import type { GraphNode } from "./node-types";
+
+
+export function isPublic(node: GraphNode): boolean {
+    return node.publicExposed === true;
+}
+
+export function isSink(node: GraphNode): boolean {
+    return node.kind === "rds";
+}
+
+export function hasVulnerabilities(node: GraphNode): boolean {
+    return (
+        node.vulnerabilities !== undefined &&
+        node.vulnerabilities?.length > 0
+    );
+}
